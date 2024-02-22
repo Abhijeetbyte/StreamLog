69547a4d3fef20c01615c5025a647d36418a828c
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2024-02-22 13:15:35 -0800

    Update StreamLog.h

--- StreamLog.h
+++ StreamLog.h
@@ -1,1 +1,19 @@
 
+#ifndef STREAMLOG_H
+#define STREAMLOG_H
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+class StreamLog {
+  public:
+    StreamLog(); // Constructor
+    void begin(uint32_t baudRate);
+    void data(unsigned long interval, ...);
+
+  private:
+    uint32_t _previousMillis;
+    bool _headersPrinted;
+};
+
+#endif
