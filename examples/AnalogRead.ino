a78a0c11c7e6380e2d3128a326215e684e7a84c9
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2024-02-22 13:14:39 -0800

    Test code

--- examples/AnalogRead.ino
+++ examples/AnalogRead.ino
@@ -1,1 +1,18 @@
 #include "StreamLog.h"
+
+StreamLog mylogger; // Initialize logger object 
+
+int LDR_sensor = A0 ; 
+
+void setup() {
+  mylogger.begin(9600); // Initialize Serial communication
+}
+
+void loop() {
+  int data1Variable = analogRead(LDR_sensor);
+  
+  // Example usage:
+  mylogger.data(5000, "Light Intensity", data1Variable);
+  // The first argument is the delay interval in milliseconds
+  
+}
