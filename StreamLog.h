bff7fdc44a7ec5b7c632ba3e0e31c6bab1e8411f
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2024-02-23 11:17:37 -0800

    Update StreamLog.h multi variable support update

--- StreamLog.h
+++ StreamLog.h
@@ -2,21 +2,28 @@
 #define STREAMLOG_H
 
 #include <Arduino.h>
+#include <stdarg.h>
 
 class StreamLog {
-  public:
-    // Constructor
-    StreamLog();
+public:
+  // Constructor: Initializes the StreamLog object
+  StreamLog();
 
-    // Function to begin serial communication
-    void begin(uint32_t baudRate);
+  // Function to begin serial communication
+  void begin(uint32_t baudRate);
 
-    // Function to log data at specified intervals
-    void data(unsigned long interval, const char* sensorName, int sensorValue);
+  // Function to print sensor headings
+  void head(const char* dataHeadings);
 
-  private:
-    // Variable to store the previous millis() value
-    unsigned long _previousMillis;
+  // Function to log data at specified intervals
+  // Parameters:
+  //   - interval: time interval between data logs in milliseconds
+  //   - numReadings: total number of sensor readings (including delay)
+  //   - ...: variable number of sensor readings
+  void data(unsigned long interval, int numReadings, ...);
+
+private:
+  unsigned long _previousMillis; // Variable to store the previous timestamp
 };
 
 #endif
