4946c65773f52ab0329025e7f3e5b6b353a7b955
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2024-02-23 09:15:36 -0800

    Update StreamLog.cpp

--- StreamLog.cpp
+++ StreamLog.cpp
@@ -1,10 +1,8 @@
-
 #include "StreamLog.h"
 #include <stdarg.h>
 
 StreamLog::StreamLog() {
   _previousMillis = 0;
-  _headersPrinted = false;
 }
 
 void StreamLog::begin(uint32_t baudRate) {
@@ -13,32 +11,24 @@ void StreamLog::begin(uint32_t baudRate) {
 
 void StreamLog::data(unsigned long interval, ...) {
   unsigned long currentMillis = millis();
-  if (!_headersPrinted) {
-    va_list headerArgs;
-    va_start(headerArgs, interval);
-    const char* name;
-
-    while ((name = va_arg(headerArgs, const char*)) != NULL) {
-      Serial.print(name);
-      Serial.print(", ");
-    }
-    Serial.println();
-    va_end(headerArgs);
-    
-    _headersPrinted = true;
-  }
-  
   if (currentMillis - _previousMillis >= interval * 1000) {
     va_list args;
     va_start(args, interval);
 
-    const char* name;
-    int value;
-    while ((name = va_arg(args, const char*)) != NULL) {
-      value = va_arg(args, int);
-      Serial.print(value);
-      Serial.print(", ");
-    }
+    // Print timestamp in seconds
+    Serial.print(currentMillis / 1000);
+    Serial.print(", ");
+
+    // Print Elapsedtime header
+    Serial.print("Elapsedtime (s), ");
+
+    // Print sensor name
+    Serial.print(va_arg(args, const char*));
+    Serial.print(", ");
+
+    // Print sensor value
+    Serial.print(va_arg(args, int));
+
     Serial.println();
     
     va_end(args);
